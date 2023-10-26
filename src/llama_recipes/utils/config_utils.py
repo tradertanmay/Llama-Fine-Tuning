b968431d61149abc5ae5cb14e0542af7359a70bc
Tanmay Sah <34967207+tradertanmay@users.noreply.github.com>
2023-10-26 12:17:44 -0400

    Update config_utils.py

--- src/llama_recipes/utils/config_utils.py
+++ src/llama_recipes/utils/config_utils.py
@@ -18,7 +18,45 @@ from llama_recipes.configs import datasets, lora_config, llama_adapter_config, p
 from llama_recipes.data.sampler import LengthBasedBatchSampler, DistributedLengthBasedBatchSampler
 from llama_recipes.utils.dataset_utils import DATASET_PREPROC
 
+from dataclasses import dataclass
+from transformers.tokenization_utils_base import PreTrainedTokenizerBase, PaddingStrategy
+from typing import Optional, Union
+import torch
 
+
+@dataclass
+class DataCollatorForMultipleChoice:
+    """
+    Data collator that will dynamically pad the inputs for multiple choice received.
+    """
+
+    tokenizer: tokenizer
+    padding: Union[bool, str, PaddingStrategy] = True
+    max_length: Optional[int] = None
+    pad_to_multiple_of: Optional[int] = None
+
+    def __call__(self, features):
+        label_name = "label" if "label" in features[0].keys() else "labels"
+        labels = [feature.pop(label_name) for feature in features]
+        batch_size = len(features)
+        num_choices = len(features[0]["input_ids"])
+        flattened_features = [
+            [{k: v[i] for k, v in feature.items()} for i in range(num_choices)] for feature in features
+        ]
+        flattened_features = sum(flattened_features, [])
+
+        batch = self.tokenizer.pad(
+            flattened_features,
+            padding=self.padding,
+            max_length=self.max_length,
+            pad_to_multiple_of=self.pad_to_multiple_of,
+            return_tensors="pt",
+        )
+
+        batch = {k: v.view(batch_size, num_choices, -1) for k, v in batch.items()}
+        batch["labels"] = torch.tensor(labels, dtype=torch.int64)
+        return batch
+        
 def update_config(config, **kwargs):
     if isinstance(config, (tuple, list)):
         for c in config:
@@ -82,7 +120,7 @@ def get_dataloader_kwargs(train_config, dataset, tokenizer, mode):
                 )
             else:
                 kwargs["batch_sampler"] = LengthBasedBatchSampler(dataset, batch_size, drop_last=True, shuffle=mode=="train")
-            kwargs["collate_fn"] = DataCollatorForSeq2Seq(tokenizer)
+            kwargs["collate_fn"] = DataCollatorForMultipleChoice(tokenizer)
         elif train_config.batching_strategy == "packing":
             if train_config.enable_fsdp:
                 kwargs["sampler"] = DistributedSampler(

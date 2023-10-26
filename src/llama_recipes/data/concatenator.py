0b355c66e0f1376d4ef06f08b726820452e58917
Tanmay Sah <34967207+tradertanmay@users.noreply.github.com>
2023-10-26 14:14:07 -0400

    Update concatenator.py

--- src/llama_recipes/data/concatenator.py
+++ src/llama_recipes/data/concatenator.py
@@ -6,50 +6,26 @@ from itertools import chain
 
 from torch.utils.data import Dataset
 
-import torch.nn.functional
-from torch.utils.data import Dataset
-import torch
-
-from torch.utils.data import Dataset
-from tqdm import tqdm
 
 class ConcatDataset(Dataset):
     def __init__(self, dataset, chunk_size=4096):
         self.dataset = dataset
         self.chunk_size = chunk_size
+
         self.samples = []
 
         buffer = {
             "input_ids": [],
             "attention_mask": [],
             "labels": [],
-        }
+            }
 
         for sample in tqdm(self.dataset, desc="Preprocessing dataset", dynamic_ncols=True):
-            input_ids = sample["input_ids"]
-            attention_mask = sample["attention_mask"]
-            labels = sample["labels"]
+            buffer = {k: v + sample[k] for k,v in buffer.items()}
 
-            # Extend the buffer lists with the current sample data
-            buffer["input_ids"].extend(input_ids)
-            buffer["attention_mask"].extend(attention_mask)
-            buffer["labels"].append(labels)  # Assuming labels is a single integer for each sample
-
-            # Check if the buffer size exceeds the chunk size, then create a chunk
-            while len(buffer["input_ids"]) >= self.chunk_size:
-                chunk = {
-                    "input_ids": buffer["input_ids"][:self.chunk_size],
-                    "attention_mask": buffer["attention_mask"][:self.chunk_size],
-                    "labels": buffer["labels"][:self.chunk_size],
-                }
-
-                # Append the chunk to samples
-                self.samples.append(chunk)
-
-                # Remove processed data from buffer
-                buffer["input_ids"] = buffer["input_ids"][self.chunk_size:]
-                buffer["attention_mask"] = buffer["attention_mask"][self.chunk_size:]
-                buffer["labels"] = buffer["labels"][self.chunk_size:]
+            while len(next(iter(buffer.values()))) > self.chunk_size:
+                self.samples.append({k: v[:self.chunk_size] for k,v in buffer.items()})
+                buffer = {k: v[self.chunk_size:] for k,v in buffer.items()}
 
     def __getitem__(self, idx):
         return self.samples[idx]
